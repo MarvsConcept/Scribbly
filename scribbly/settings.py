2bc60d61bd6f661818dd8345580a004f046cbead
MarvsConcept <marvade8@gmail.com>
2025-01-04 10:58:18 +0100

    fixed the base

--- scribbly/settings.py
+++ scribbly/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'notes.apps.NotesConfig',
+    'users.apps.UsersConfig',
+
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -123,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKs = 'bootstrap4'
