d5b3778f468426929a56c497f8768faccdc42934
MarvsConcept <marvade8@gmail.com>
2025-01-04 14:00:29 +0100

    Added Login and Logout

--- users/views.py
+++ users/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Account created successfully for {username}!")
-            return redirect('notes/home.html')
-
+            messages.success(request, f"Accounted Created Successfully for {username}!")
+            return redirect('home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get('username')
+#             messages.success(request, f"Account created successfully for {username}!")
+#             return redirect('notes/home.html')
+
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'users/register.html', {'form': form})
