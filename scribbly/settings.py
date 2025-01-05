67162d2c94589d6b3de77c54a4c2f2086be7035a
MarvsConcept <marvade8@gmail.com>
2025-01-05 23:59:50 +0100

    Fixed Redirect Bug

--- scribbly/settings.py
+++ scribbly/settings.py
@@ -132,3 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 CRISPY_ALLOWED_TEMPLATE_PACKs = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = 'home'
+
+LOGIN_URL = 'login'
