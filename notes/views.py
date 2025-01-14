39eb8dbe00861a4897de7226d3140a5cfa42d5fa
MarvsConcept <marvade8@gmail.com>
2025-01-14 02:02:08 +0100

    Added Listview

--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Notes
+from django.views.generic import ListView
 
 # Create your views here.
 
-def home(request):
-    context = {
-        'notes': Notes.objects.all()
-        }
-    return render(request, 'notes/home.html', context )
+# def home(request):
+#     context = {
+#         'notes': Notes.objects.all()
+#         }
+#     return render(request, 'notes/home.html', context )
+
+class NoteListView(ListView):
+    model = Notes
+    template_name = 'notes/home.html'
+    context_object_name = 'notes'
+    ordering = ['-date_posted']
