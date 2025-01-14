b8c3c77ed9cf9ce67098e456dcb898659da3db1b
MarvsConcept <marvade8@gmail.com>
2025-01-14 17:25:51 +0100

    Added User Specific Posts

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Notes
 from django.views.generic import (
@@ -9,6 +9,7 @@ from django.views.generic import (
     DeleteView,
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -24,6 +25,17 @@ class NoteListView(ListView):
     context_object_name = 'notes'
     ordering = ['-date_posted']
     paginate_by = '5'
+
+
+class UserNoteListView(ListView):
+    model = Notes
+    template_name = 'notes/user_notes.html'
+    context_object_name = 'notes'
+    paginate_by = '5'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Notes.objects.filter(author=user).order_by('-date_posted')
 
 class NoteDetailView(DetailView):
     model = Notes
